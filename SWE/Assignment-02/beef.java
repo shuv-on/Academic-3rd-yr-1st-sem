3d2798cd2a7fe005ef60f2203c1468cdf7b7ca55
Md Shishir Kaysar Shuvon <148075201+shuv-on@users.noreply.github.com>
2025-10-18 13:34:14 +0600

    Add beef class with setter and getter for type

---
+++ SWE/Assignment-02/beef.java
@@ -0,0 +1,12 @@
+//Using Setter & Getter
+public class beef {
+    private String type;
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getType() {
+        return type;
+    }
+}
